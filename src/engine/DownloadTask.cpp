5905e1f0ed63aabf2d44f4c747adc07269a6e059
ws3917 <ws3917@outlook.com>
2025-12-26 10:29:52 +0800

    .

---
+++ src/engine/DownloadTask.cpp
@@ -0,0 +1,37 @@
+﻿#include "DownloadTask.h"
+#include <cpr/cpr.h>
+DownloadTask::DownloadTask(const std::string &url,
+                           std::function<void(const std::string)> onSuccess,
+                           std::function<void(const std::string)> onError,
+                           int maxRetries) {
+  progress = 0;
+  state = DownloadState::RUNNING;
+  impl = std::jthread([=](std::stop_token stoken) {
+    for (int retry = 0;; retry++) {
+      cpr::Response r = cpr::Get(
+          cpr::Url(url),
+          cpr::ProgressCallback([this, &stoken](size_t total, size_t now,
+                                                size_t, size_t, intptr_t) {
+            if (total == 0 || stoken.stop_requested()) {
+              return false;
+            }
+            progress = now * 100 / total;
+            return true;
+          }));
+      if (stoken.stop_requested()) {
+        onError("Download task cancelled.");
+        state = DownloadState::FAILED;
+        return;
+      } else if (r.error.code == cpr::ErrorCode::OK && r.status_code == 200) {
+        onSuccess(result);
+        state = DownloadState::FINISHED;
+        return;
+      } else if (++retry == maxRetries) {
+        onError("HTTP error: " + std::to_string(r.status_code) +
+                ", please check your network, or download the offline patch.");
+        state = DownloadState::FAILED;
+        return;
+      }
+    }
+  });
+}
---
+++ src/object/OPatchValue.h
@@ -0,0 +1,22 @@
+﻿#ifndef O_CHECK_TOOL
+#define O_CHECK_TOOL
+#include "../engine/GameObject.h"
+#include "../engine/GameConfig.h"
+#include "../engine/DownloadTask.h"
+#include <memory>
+class OPatchValue : GameObject {
+public:
+  enum class PatchValueState { NotReady, Ready, Failed };
+  OPatchValue();
+  void Draw() override {}
+  void Update(float dt) override {};
+  PatchValueState GetState();
+  bool Download();
+  std::string GetValue(std::string key);
+
+private:
+  std::unique_ptr<DownloadTask> downloadTask;
+  PatchValueState currentState = PatchValueState::NotReady;
+  GameConfig config;
+};
+#endif
